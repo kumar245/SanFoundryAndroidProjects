201ca27e062bf898b5b1b66fed8778d32eeb6796
chaitanya Kumar <popurichaitanyakumar@gmail.com>
2018-04-06 18:40:54 -0400

    Completed ScorllView

--- scrollview/src/main/java/com/example/scrollview/MainActivity.java
+++ scrollview/src/main/java/com/example/scrollview/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -39,7 +40,36 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.button1:
-                startActivity(new Intent());
+                Toast.makeText(this, "This is First Button", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.button2:
+                Toast.makeText(this, "This is Second Button", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.button3:
+                Toast.makeText(this, "This is Third Button", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.button4:
+                Toast.makeText(this, "This is Fourth Button", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.button5:
+                Toast.makeText(this, "This is Fifth Button", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.button6:
+                Toast.makeText(this, "This is Sixth Button", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.button7:
+                Toast.makeText(this, "This is Seventh Button", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.button8:
+                Toast.makeText(this, "This is Eighth Button", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.button9:
+                Toast.makeText(this, "This is Ninth Button", Toast.LENGTH_LONG).show();
+                break;
+                case R.id.button10:
+                Toast.makeText(this, "This is Tenth Button", Toast.LENGTH_LONG).show();
+                break;
+
         }
 
     }
