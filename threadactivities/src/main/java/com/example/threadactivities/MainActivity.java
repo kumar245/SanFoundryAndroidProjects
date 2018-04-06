d0508a4e41a6336e7d63ab0ba2b0b95eb1991143
chaitanya Kumar <popurichaitanyakumar@gmail.com>
2018-04-06 19:24:20 -0400

    Completed ThreadActivity

---
+++ threadactivities/src/main/java/com/example/threadactivities/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.threadactivities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+       Thread thread=new Thread(){
+           @Override
+           public void run() {
+               super.run();
+               try {
+                   sleep(2000);
+                   Intent intent=new Intent(MainActivity.this,SecondActivity.class);
+                   startActivity(intent);
+               } catch (InterruptedException e) {
+                   e.printStackTrace();
+               }
+
+           }
+       };
+       thread.start();
+
+
+    }
+}
