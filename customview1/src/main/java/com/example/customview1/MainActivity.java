5eb6983056def57ea22d05716f44aa3947c204fa
chaitanya Kumar <popurichaitanyakumar@gmail.com>
2018-04-09 04:40:05 -0400

    Completed FIrst CustomView

---
+++ customview1/src/main/java/com/example/customview1/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.customview1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+
+    public void onClicked(View view) {
+        String text = view.getId() == R.id.view1 ? "Background" : "Foreground";
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+    }
+}
