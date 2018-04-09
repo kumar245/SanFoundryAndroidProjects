3bc7719cd46ee7179bc8061a91f2fe522c255c47
chaitanya Kumar <popurichaitanyakumar@gmail.com>
2018-04-08 22:43:39 -0400

    Completed Dialog

---
+++ alertdialog/src/main/java/com/example/alertdialog/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.alertdialog;
+
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button=findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder=new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Alert Dialog");
+                builder.setMessage("Whats Up");
+                builder.setPositiveButton("OK",null);
+                AlertDialog alertDialog= builder.create();
+                alertDialog.show();
+
+                finish();
+            }
+        });
+
+
+    }
+}
